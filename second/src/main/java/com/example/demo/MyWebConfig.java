054791f9bab47a70a78ab8a6a8db9d3de264a62c
mjkim856 <mjkim856@gmail.com>
2023-02-03 13:52:45 +0900

    MyWebConfig.java (업로드 경로를 ec2서버의 /usr/upload로 변경)

--- second/src/main/java/com/example/demo/MyWebConfig.java
+++ second/src/main/java/com/example/demo/MyWebConfig.java
@@ -6,11 +6,20 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration // 나는 설정파일
 public class MyWebConfig implements WebMvcConfigurer {
+	
+	//for window
+	// static final public String savePath = "c:/upload/";
+	
+	//for window
+	static final public String savePath = "/usr/upload/";
+
+	// <resources mapping="/upload/**" location="file:///c:upload/" />
 
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		// <resources mapping="/upload/**" location="file:///c:upload/" />
-		registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
+		// registry.addResourceHandler("/upload/**").addResourceLocations("file:///c:/upload/");
+		registry.addResourceHandler("/upload/**").
+		addResourceLocations("file:/usr/upload/");
 	}
 
 }
